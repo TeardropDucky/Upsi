4ff0673c49ee2a4350d0ade92f19256f3f2fb88b
TeardropDucky <himmelherren@hotmail.com>
2018-07-29 11:01:44 +0200

    Started work on adding letters

--- DuckyPlayzone/Letters/Letters/Letters/LetterList.cs
+++ DuckyPlayzone/Letters/Letters/Letters/LetterList.cs
@@ -45,5 +45,10 @@ namespace Letters
 		{
 			
 		}
+
+		public bool Save()
+		{
+			return true;
+		}
 	}
 }
--- DuckyPlayzone/Letters/Letters/Letters/Program.cs
+++ DuckyPlayzone/Letters/Letters/Letters/Program.cs
@@ -12,6 +12,9 @@ namespace Letters
         static void Main(string[] args)
         {
 			bool end = false;
+			var list = new LetterList();
+			var history = new List<Tuple<char,string>>();
+
 			string menu = @"
 ********************
 *       Menu       *
@@ -19,31 +22,55 @@ namespace Letters
 *    1: Tilføj     *
 *    2: Fjern      *
 *    3: Print      *
-*    4: Luk        *
+*    4: Undo       *
+*    5: Luk        *
 ********************
 ";
 			while (!end)
 			{
 				Console.WriteLine(menu);
 				string input = Console.ReadLine().ToLower();
+				history.Add(new Tuple<char, string>('m',input));
+
 				switch (input)
 				{
 					case "1":
 					case "tilføj":
 					case "tilfoj":
-						Console.Clear();
+						Console.WriteLine("Input tilføjelser og tryk enter:");
+						input = Console.ReadLine();
+						if(input == "tilbage")
+						{
+							history.Add(new Tuple<char, string>('m', input));
+						}
+						else
+						{
+							foreach(var item in input)
+							{
+								list.AddLetter(item);
+							}
+						}
 						break;
 					case "2":
 					case "fjern":
-						Console.Clear();
 						break;
 					case "3":
 					case "print":
-						Console.Clear();
 						break;
 					case "4":
+					case "undo":
+						break;
+					case "5":
 					case "luk":
-						end = true;
+						if(list.Save())
+						{
+							end = true;
+						}
+						else
+						{
+
+						}
+							
 						break;
 					default:
 						Console.WriteLine("Input ikke forstået");
